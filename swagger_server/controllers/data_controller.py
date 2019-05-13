02e36aa67c8891af12ebb84daa1e3d84c133f89e
bongi23 <andrea.bongi@live.it>
2019-05-13 11:46:40 +0200

    data controller minor fix

--- swagger_server/controllers/data_controller.py
+++ swagger_server/controllers/data_controller.py
@@ -30,28 +30,35 @@ def get_data(plant_id, sensor=None, min_value=None, max_value=None, min_time=Non
     if plant is None:
         abort(404)
     query = dict()
-    query['sensor'] = sensor
 
-    if min_value > max_value or max_time < min_time:
-        abort(400)
+    query['microbit'] = plant_id
 
-    elif min_value == 0 and max_value != 0:
-        query['value'] = {'$lt': max_value}
-    elif min_value != 0 and max_value == 0:
-        query['value'] = {'$gt': min_value}
-    elif min_value != 0 and max_value != 0:
-        query['value'] = {'$and': [{'$lt': max_value}, {'$gt': min_value}]}
+    if sensor is not None:
+        query['sensor'] = sensor
 
-    if min_time == 0 and max_time > 0:
-        query['time'] = {'$lt': max_value}
-    elif min_time > 0 and max_value == 0:
-        query['time'] = {'$gt': min_value}
-    elif min_value != 0 and max_value != 0:
-        query['time'] = {'$and': [{'$lt': max_value}, {'$gt': min_value}]}
+        if min_value is None and max_value is not None:
+            query['value'] = {'$lt': max_value}
+        elif min_value is not None and max_value is None:
+            query['value'] = {'$gt': min_value}
+        if min_value is not None and max_value is not None:
+            if min_value > max_value:
+                abort(400)
+            else:
+                query['value'] = {'$and': [{'$lt': max_value}, {'$gt': min_value}]}
+
+        if min_time is None and max_time is not None:
+            query['timestamp'] = {'$lt': max_time}
+        elif min_time is not None and max_time is None:
+            query['timestamp'] = {'$gt': min_time}
+        if min_time is not None and max_time is not None:
+            if min_time > max_time:
+                abort(400)
+            else:
+                query['timestamp'] = {'$and': [{'$lt': max_time}, {'$gt': min_time}]}
 
     data = util.get_collection('data')
     res = []
-    for d in data.find(query):
+    for d in data.find(query).limit(10):
         del d['_id']
         res.append(d)
     return res
