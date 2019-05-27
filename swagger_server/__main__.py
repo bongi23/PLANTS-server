53ae5dc8577d68879b08234022fe58613d819f3d
bongi23 <andrea.bongi@live.it>
2019-05-27 10:00:04 +0200

    removed CORS

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 import connexion
-from flask_cors import CORS
 
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.add_api('swagger.yaml', arguments={'title': 'P.L.A.N.T.S. API'})
-    CORS(app.app)
     app.run(port=8080, debug=True)
 
 
