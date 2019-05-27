2e75aa7cafc505882307f4228dcbe2a4b8a12107
William Guglielmo <william@deselmo.com>
2019-05-27 04:53:16 +0200

    Add cors

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import connexion
+from flask.ext.cors import CORS
 
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.add_api('swagger.yaml', arguments={'title': 'P.L.A.N.T.S. API'})
+    CORS(app.app)
     app.run(port=8080, debug=True)
 
 
